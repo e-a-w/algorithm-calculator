83a800eb59bbacb4f978e86eb0e424443e61a12b
e-a-w- <eawatkins.0@gmail.com>
2020-12-31 07:21:35 -0500

    update script

--- script.js
+++ script.js
@@ -39,16 +39,26 @@ class RuntimeCalc extends Calculator {
     return i;
   }
 
-  calculate() {
+  calcAll() {
     const c = Calculator.calculators[0].value;
     const counters = [];
-    counters.push(c);
-    counters.push(this.value);
-    counters.push(this.logN(this.value).toFixed(5));
-    counters.push(this.value ** 2);
-    counters.push(this.value * this.logN(this.value).toFixed(5));
-    counters.push(this.factorial());
+    [1, 2].forEach(x => {
+      counters.push(
+        c,
+        this.logN(this.value).toFixed(5),
+        this.value,
+        (this.value * this.logN(this.value)).toFixed(5),
+        this.value ** 2,
+        this.value ** 3,
+        2 ** this.value,
+        this.factorial()
+      );
+    });
     return counters;
+  }
+
+  calculate() {
+    return this.calcAll();
   }
 }
 
@@ -80,7 +90,22 @@ class NLogCalc extends Calculator {
   }
 }
 
+class ExpCalc extends Calculator {
+  constructor(idx, value, max, min) {
+    super(idx, value, max, min);
+  }
+
+  calcExp() {
+    return this.value ** Calculator.calculators[1].value;
+  }
+
+  calculate() {
+    return [this.calcExp()];
+  }
+}
+
 const c0 = new ConstCalc(1, 1, 10000, 0);
 const c1 = new RuntimeCalc(0, 3, 10000, 0);
 const c2 = new LogCalc(2, 2, 10000, 0);
 const c3 = new NLogCalc(3, 2, 10000, 0);
+const c4 = new ExpCalc(4, 2, 10000, 0);
